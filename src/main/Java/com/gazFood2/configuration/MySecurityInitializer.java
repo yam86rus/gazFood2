dd9683071cdb66859d52b512b0de4efae1eee1c0
Yam86rus <yam86rus@gmail.com>
2021-03-15 09:51:54 +0500

    first commit

---
+++ src/main/Java/com/gazFood2/configuration/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.gazFood2.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
